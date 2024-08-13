35a53fba4749001bbd0d656e70110d532d25c9f6
Etim Precious <123295022+petim7277@users.noreply.github.com>
2024-08-13 15:53:47 +0100

    Responsive Navigation sectiion finished.

--- src/Components/Reuseables/NavBar/NavBar.jsx
+++ src/Components/Reuseables/NavBar/NavBar.jsx
@@ -1,16 +1,23 @@
 import  styles from "./NavBar.module.css"
 import logoImage from "../../../Assets/happyemoji.png"
+import {useState} from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
   return(
       <div className={styles.NavCont}>
+
             <div className={styles.NavLogo}>
                 <img src={logoImage} alt={"CompanyLogo"}/>
                 <p>Helpeveryone</p>
             </div>
-
-          <div className={styles.NavLinks}>
+      <div className={styles.wrapDiv}>
+          <div className={`${styles.NavLinks}${menuOpen?styles.NavLinks:""}`}>
               <p ><span>Services</span></p>
               <p>Donate</p>
               <p>Who we are</p>
@@ -19,10 +26,18 @@ const NavBar = () => {
               <p>Blog</p>
           </div>
 
-          <div className={styles.Button}>
-              <button>Sign up</button>
+          <div className={`${styles.Button}${menuOpen?styles.Button:""}`}>
+              <button className={`${styles.button}${menuOpen?styles.button:""}`}>Sign up</button>
+          </div>
+
+      </div>
+          <div className={styles.Hamburger} onClick={toggleMenu}>
+              {menuOpen ? <FaTimes /> : <FaBars />}
           </div>
       </div>
   )
 }
 export  default  NavBar;
+
+
+
