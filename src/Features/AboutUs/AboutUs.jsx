a6e9ce0516c1e564b5286273d56f2536d53efd99
Etim Precious <123295022+petim7277@users.noreply.github.com>
2024-08-16 10:45:38 +0100

    finished mobile responsive ness forallScreens

--- src/Features/AboutUs/AboutUs.jsx
+++ src/Features/AboutUs/AboutUs.jsx
@@ -5,21 +5,17 @@ const  AboutUs = ()=>{
     return(
         <div className={styles.mainDiv}>
                     <div className={styles.firstDiv}>
-                        <img src={sunnyClouds} alt={"Image of sunny clouds"}/>
+                        <img src={sunnyClouds} alt={"Image of sunny clouds"} />
                         <h1> About us</h1>
                     </div>
-
                     <div className={styles.secondDiv}>
-                    <p>Lorem ipsum dolor sit amet consectetur. Id perambulator oculist latest exists valid phallus<br/>
-                        euismod aliquot. Ullamcorper sit amet deque tells libero sem dui tristique. Ac quam penatibus velit <br/>
-                                                                                     at ipsum aliquet tortor</p>
+                        <p>Lorem ipsum dolor sit amet consectetur. Id perambulator oculist latest exists valid phallus<br/>
+                        euismod aliquot. Ullamcorper sit amet deque tells libero sem dui tristique. Ac quam penatibus velit <br/>at ipsum aliquet tortor</p>
                     </div>
                     <div className={styles.tentImage}>
-                        <img src={tents} alt={"image of tents"}/>
-
+                        <img src={tents} alt={"image of tents"} />
                     </div>
-
         </div>
     )
-}
+};
 export  default  AboutUs;
